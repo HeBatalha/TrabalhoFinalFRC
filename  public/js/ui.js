4b805f3ae1a6a286f05a13bdaa31212cb0ea8eca
yagompassos <yagompassos@gmail.com>
2024-09-15 17:59:22 -0300

    Removendo arquitetura que está ná branch arquitetura-js

---  public/js/ui.js
+++
@@ -1,8 +0,0 @@
-export function hideElement(selector) {
-    document.querySelector(selector).style.display = 'none';
-  }
-  
-  export function showElement(selector) {
-    document.querySelector(selector).style.display = 'block';
-  }
-  
